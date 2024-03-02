7f5dfaea4cdaca5f0b9118e4c1008b10ad9abaa1
NoBugInMyCode <knysun@ucdavis.edu>
2024-03-02 03:40:06 -0800

    update

--- README.md
+++ README.md
@@ -8,8 +8,6 @@ GraphQL
 1. Make sure you have installed bazel5.0 and pybind11
 2. Run command `bazel build :pybind_kv_so`
 3. From `kv_operation.py` import `get_value` and `set_value` function into your Python file to use it
-4. Parameters:
-   1. key: The key of the value you want to set or get
-   2. value: The key's corresponding value you want to set
-   3. config_path: THe path to config file of the blockchain(For main chain the config file should be `5 44.193.63.142 17005`)
-   This hasn't to be a absolute path.
+
+## Example
+TODO
--- kv_operation.py
+++ kv_operation.py
@@ -6,10 +6,23 @@ import pybind_kv
 
 
 def set_value(key: str or int or float, value: str or int or float, config_path: str) -> bool:
+    """
+
+    :param key: The key you want to set your value to.
+    :param value: The key's corresponding value in key value pair.
+    :param config_path: The path to .config file of the blockchain(For main chain the config file should be `5 44.193.63.142 17005`)
+    :return: True if value has been set successfully.
+    """
     return pybind_kv.set(str(key), str(value), os.path.abspath(config_path))
 
 
 def get_value(key: str or int or float, config_path: str) -> str:
+    """
+
+    :param key: The key of the value you want to get in key value pair.
+    :param config_path: The path to .config file of the blockchain(For main chain the config file should be `5 44.193.63.142 17005`)
+    :return: A string of the key's corresponding value.
+    """
     return pybind_kv.get(str(key), os.path.abspath(config_path))
 
 
